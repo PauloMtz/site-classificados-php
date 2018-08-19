ee3b95e0c77f231fb9517a4dc29a77bbdf547158
PauloMtz <pramosmtz@gmail.com>
2018-08-19 19:26:48 -0300

    Implementação da edição do anúncio

--- classes/Anuncio.class.php
+++ classes/Anuncio.class.php
@@ -1,6 +1,7 @@
 <?php
 class Anuncio {
 
+	// esse método é utilizado na página para exibir os anúncios do usuário (meus-anuncios.php)
 	public function getMeusAnuncios() {
 
 		global $pdo;
@@ -24,6 +25,25 @@ class Anuncio {
 		return $array;
 	}
 
+	// esse método será usado para recuperar os dados do formulário quendo da edição
+	public function getAnuncio($id) {
+
+		$array = array();
+
+		global $pdo;
+
+		$sql = $pdo->prepare("SELECT * FROM anuncios WHERE id_anuncio = :id");
+		$sql->bindValue(":id", $id);
+		$sql->execute();
+
+		if ($sql->rowCount() > 0) {
+			$array = $sql->fetch();
+		}
+
+		return $array;
+	}
+
+	// método para inserir anúncio no banco de dados
 	public function addAnuncio($categoria, $titulo, $descricao, $valor, $estado) {
 
 		global $pdo;
@@ -35,6 +55,22 @@ class Anuncio {
 		$sql->bindValue(":descricao", $descricao);
 		$sql->bindValue(":valor", $valor);
 		$sql->bindValue(":estado", $estado);
+		$sql->execute();
+	}
+
+	// esse método foi copiado do addAnuncio e apenas adicionado o id
+	public function editAnuncio($categoria, $titulo, $descricao, $valor, $estado, $id) {
+
+		global $pdo;
+
+		$sql = $pdo->prepare("UPDATE anuncios SET usuario_id = :usuario, categoria_id = :categoria, titulo = :titulo, descricao = :descricao, valor = :valor, estado = :estado WHERE id_anuncio = :id");
+		$sql->bindValue(":usuario", $_SESSION['clogin']);
+		$sql->bindValue(":categoria", $categoria);
+		$sql->bindValue(":titulo", $titulo);
+		$sql->bindValue(":descricao", $descricao);
+		$sql->bindValue(":valor", $valor);
+		$sql->bindValue(":estado", $estado);
+		$sql->bindValue(":id", $id);
 		$sql->execute();
 	}
 
---
+++ editar-anuncio.php
@@ -0,0 +1,95 @@
+<?php require 'temp/header.php';
+
+// verifica se existe sessão
+if (empty($_SESSION['clogin'])) {
+	?>
+	<script type="text/javascript">window.location.href="login.php";</script>
+	<?php
+	exit;
+}
+
+// chama a classe Anuncio
+require 'classes/Anuncio.class.php';
+
+// cria objeto
+$a = new Anuncio();
+
+// consultar o banco para recuperar os dados preenchidos no formulário
+// verifica se foi enviado um id a partir do formulário
+if (isset($_GET['id'])) {
+
+	$id = $_GET['id'];
+
+	// pega o id passado e joga no método getAnuncio que está na classe Anuncio
+	$dados = $a->getAnuncio($id);
+} else {
+	?>
+	<script type="text/javascript">window.location.href="meus-anuncios.php";</script>
+	<?php
+	exit;
+}
+
+// depois de consultar, receber os dados para atualização no banco
+if(isset($_POST['titulo']) && !empty($_POST['titulo'])) {
+
+	// pega os dados e joga nas variáveis
+	$categoria = addslashes($_POST['categoria']);
+	$titulo = addslashes($_POST['titulo']);
+	$valor = addslashes($_POST['valor']);
+	$descricao = addslashes($_POST['descricao']);
+	$estado = addslashes($_POST['estado']);
+
+	// pega as variáveis e joga no método editAnuncio, que fica na classe Anuncio
+	$a->editAnuncio($categoria, $titulo, $descricao, $valor, $estado, $id);
+	
+	?>
+	<div class="alert alert-success">
+		Anúncio atualizado com sucesso!
+	</div>
+	<?php
+}
+?>
+<div class="container">
+	<h2>Editar Anúncio</h2><hr>
+	<form method="POST" enctype="multipart/form-data">
+		<div class="form-group">
+			<label for="titulo">Titulo</label>
+			<input type="text" name="titulo" id="titulo" class="form-control" autofocus="true" value="<?php echo $dados['titulo'] ?>">
+		</div>
+		<div class="form-group">
+			<label for="descricao">Descrição</label>
+			<textarea class="form-control" name="descricao"><?php echo $dados['descricao'] ?></textarea>
+		</div>
+		<div class="row">
+			<div class="form-group col-sm-4">
+				<label for="estado">Estado de conservação</label>
+				<select name="estado" id="estado" class="form-control">
+					<option value="1"<?php echo ($dados['estado'] == 1) ? 'selected="selected"' : '' ?>>Ruim</option>
+					<option value="2"<?php echo ($dados['estado'] == 2) ? 'selected="selected"' : '' ?>>Bom</option>
+					<option value="3"<?php echo ($dados['estado'] == 3) ? 'selected="selected"' : '' ?>>Ótimo</option>
+				</select>
+			</div>
+			<div class="form-group col-sm-4">
+				<label for="valor">Valor</label>
+				<input type="text" name="valor" id="valor" class="form-control" value="<?php echo $dados['valor'] ?>">
+			</div>
+			<div class="form-group col-sm-4">
+				<label for="categoria">Categoria</label>
+				<select name="categoria" id="categoria" class="form-control">
+					<?php
+					require "classes/Categoria.class.php";
+					$c = new Categoria();
+					$categorias = $c->getList();
+					foreach ($categorias as $categoria) {
+					?>
+					<option value="<?php echo $categoria['id_categorias'] ?>"<?php echo ($dados['categoria_id'] == $categoria['id_categorias']) ? 'selected="selected"' : '' ?>><?php echo utf8_encode($categoria['nome']) ?></option>
+					<?php
+					}
+					?>
+				</select>
+			</div>
+		</div>
+		<input type="submit" value="Atualizar">
+	</form>
+</div>
+<?php require 'temp/footer.php'; ?>
